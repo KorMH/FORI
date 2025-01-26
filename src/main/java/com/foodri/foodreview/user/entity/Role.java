a24f19faae224a8aaa6fafe2fa3869e27ed72f2e
KorMH <dvkk12@gmail.com>
2025-01-26 23:38:07 +0900

    변경사항 AS-IS
    TO-BE
    USER entity 생성
    build.gradle jsontoken 추가

---
+++ src/main/java/com/foodri/foodreview/user/entity/Role.java
@@ -0,0 +1,5 @@
+package com.foodri.foodreview.user.entity;
+
+public enum Role {
+  ROLE_USER, ROLE_OWNER
+}
---
+++ src/main/java/com/foodri/foodreview/user/entity/User.java
@@ -0,0 +1,45 @@
+package com.foodri.foodreview.user.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user")
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false, unique = true)
+  private String email;
+
+  @Column(nullable = false)
+  private String password;
+
+  @Column(nullable = false, unique = true)
+  private String phone_number;
+
+  @Column(nullable = false)
+  private Role role;
+
+  @Column(nullable = false)
+  private LocalDateTime create_at;
+
+  @Column(nullable = false)
+  private LocalDateTime update_at;
+}
